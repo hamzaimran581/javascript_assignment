38a5e347b6407add61555fe9e1ab1fae7e3aeadf
hamzaimran581 <hamzaimran581@gmail.com>
2020-06-14 01:13:06 +0500

    chapter 1 to 10 completed

---
+++ app.js
@@ -0,0 +1,348 @@
+// //chapter 1 
+
+// //task 1 
+// alert ("hello world");
+
+// //task 2
+// alert ("Error! Please enter a valid password"); 
+
+// //task 3 
+// alert ("Welcome to js land...\nhappy coding!");
+
+// //task 4
+// alert ("Welcome to js land..."); 
+
+
+
+// //chapter 2
+
+// //task 1
+// var username = "";
+
+// //task 2
+// var myname = "Muhammad Hamza Imran";
+
+// //task 3 a,b
+// var message = "Hello World"; 
+
+// //task 3 c
+// alert (message);
+
+// //task 4
+// var student_name = "jon doe";
+// var student_age = "15 year old";
+// var student_course = "certified mobile application development";
+// alert (student_name);
+// alert (student_age);
+// alert (student_course);
+
+// //task 5
+// alert ("Pizza\nPizz\nPiz\nPi\nP " );
+
+// //task 6
+// var email = "hamzaimran581@gmail.com";
+// alert ("My email address is "  + email );
+
+// //task 7
+// var book = "A smarter way to learn JavaScript";
+// alert ("Iam trying to learn from " + book);
+
+// //task 8
+// document.write ( "Yeah I can write html content from javasript" )
+
+// //task 9
+// var dont_know_what_is_this =" ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬  ";
+// alert (dont_know_what_is_this);
+
+
+
+
+// //chapter 3 
+
+// //task 1
+// var age = 15;
+// alert ("I am " + age + " years old");
+
+// //task 2
+// var n = 203;
+// alert ("you have visited this site " + n + " times");
+
+// //task 3
+// var birthyear = 2011;
+// document.write ("my birth year is " + birthyear );
+// document.write (" </br> data type of my declared variable is number ");
+
+// //task 4
+// var costumer = "jon doe"
+// var x = 4;
+// var product = "tshirt"
+// document.write ( "</br>" + costumer + " ordered " +  x  +  " " +  product + " on xyz store"  );
+
+
+// //chapter 4 
+// //task 1
+
+
+// //task 2
+// var n = "number";
+// var $ = "$";
+// var _ = "_";
+// var caseOfvar = "sensitive";
+// var noKeyword = "js keyboard";
+// // var var = "illegal";
+// // var case = "also not illegal";
+// // var na me = "not allowed";
+// // var % = "% could not be use";
+// // var & = "illegal";
+
+// //task 3
+// document.write (  "<h1> Rules for naming JS variables </h1> <br>" +
+// "Variable names can only contain " + n + " " + $ + " " + _ 
+// + " .For example $my_1stVariable <br> Variables must begin with a letter , $ or _. For example $name, _name or name d"
+// + "<br> Variable names should not be " + noKeyword );
+
+
+// //chapter 5
+
+// //task 1
+// var x = 5;
+// var y = 3;
+// var z = x + y; 
+// document.write ("the sum of " + x + " and " + y + " is " +  z );
+
+// //task 2
+// var x = 5;
+// var y = 3;
+// var z = x - y; 
+// document.write ("<br> the substraction of " + x + " and " + y + " is " +  z );
+
+
+// var x = 5;
+// var y = 3;
+// var z = x * y ;
+// document.write ("<br> the multipication of " + x + " and " + y + " is " +  z );
+
+
+// //task 3
+
+// //a , c
+// var x = 5 ;
+
+
+// //b
+// document.write ("Value after variable declaration is: " + x ); 
+
+// //d
+// document.write ("<br> Initial value: " + x); 
+
+// //e , f
+// document.write ("<br> Value after increment is " + ++x);
+
+// //g
+// var y = x+7
+// document.write ("<br> after addition is: " + y  );
+
+// //i
+// var z = --y
+
+// //j
+// document.write ("<br> Value after decrement is:  " + z );
+
+// //k
+// document.write ("<br>the remainder is " + z / 3 );
+
+// //task 4 
+// var ticketPrice = 600
+// document.write ("<br>total cost to buy 5 ticket to a movie is " + ticketPrice*5 );
+
+// //task 5
+// document.write ("<h1> Table of 4 </h1>"  + 
+// "4x1=4" + "<br>4x2="+4*2 + "<br>4x3="+4*3 + "<br>4x4="+4*4 + "<br>4x5="+4*5 + "<br>4x6="+4*6 
+// + "<br>4x7="+4*7+ "<br>4x8="+4*8+ "<br>4x9="+4*9+ "<br>4x10="+4*10 );
+
+// //task 6
+// var C = 36;
+// var F = (C* 9/5)+32;
+// document.write (C +"C° is " + F + "f°");
+// var f = 70;
+// var c = (f - 32)*5/9 ;
+// document.write ("<br>" + f +"f° is " + c + "C°");
+
+// //task 7
+// var p1 = 650;
+// var p2 = 100;
+// var sc = 100;
+// document.write ("Price of item 1 is " + p1);
+// document.write ("<br>quantity of item 1 is 3" );
+// document.write ("<br>Price of item 2 is " + p2);
+// document.write ("<br>quantity of item 2 is 7 ");
+// document.write ("<br>Shipping charges" + sc);
+// var tc =  ( (p1*3) + (p2*7) + 100 ); 
+// document.write ("<br>Total cost of your order is " + tc);
+
+// //task 8 
+// var tm = 980;
+// var mo = 804;
+// var per = ( (804/980) * 100);
+// document.write ("<br>Total marks: " + tm);
+// document.write ("<br>Marks Obtained " + mo);
+// document.write ("<br>Percentage: " +per);
+
+// //task 9
+// var usd = 104.80;  
+// var riyal = 28;
+// document.write ("<br> Total currency in pkr " + ( 10*usd + 25*riyal) );
+
+// //task 10;
+// var x = 10;
+// var  y = (x  + 5 * 10 / 2  );
+// document.write ("<br>" + y  );
+
+// //task 11
+// var year = 2020;
+// var yob = 1947;
+// var age = (year - yob);
+// document.write ("<br>Current year: " + year);
+// document.write ("<br>Birth year: " + yob);
+// document.write ("<br>Your current age: " + age);
+
+// //task 12
+// document.write ("<h1>The geometrizer  </h1>");
+// var r =20;
+// var c = ( 2 * 3.142 * r );
+// document.write ("<br>The radius of a circle:" +r);
+// document.write ("<br>The circumference is:" +r);
+// var area = (3.142 * r * r);
+// document.write ("<br>The area of a circle " +area);
+
+// //task 13
+// var fav = "biscuit";
+// var age = 13;
+// var maxage = 87;
+// var amount = 2;
+// var total = ( (87-13) *365 *2 );
+// document.write ("<br>FAvourute snack: " + fav);
+// document.write ("<br>current age: " +age);
+// document.write ("<br>Maximum estimated age " +maxage);
+// document.write ("<br>Amount of snack per day " +amount);
+// document.write ("<br>you will need " +total + " to last until the ripeoff age of " + maxage);
+
+// //chapter 6-9
+// //task 1
+// var a = 10;
+// document.write ("The value of a is: " + a);
+// document.write ("<hr>The value of ++a is: " + (++a));
+// document.write ("<br>now the value of a is: " + a);
+// document.write ("<br><br> the value of a++ is: " + (a++));
+// document.write ("<br>now the value of a is: " + a);
+// document.write ("<br><br> the value of --a is: " + (--a));
+// document.write ("<br>now the value of a is: " + a);
+// document.write ("<br><br> the value of a-- is: " + (a--));
+// document.write ("<br>now the value of a is: " + a);
+
+// //task 2
+// var a = 2, b = 1;
+// document.write("<br>a is " + a);
+// document.write("<br>b is " + b);
+// var result = --a - --b + ++b + b--; 
+// document.write("<br>result is " + result);
+
+// //task 3
+// var a = prompt ("enter your name");
+// alert ("Welcome "+ a);
+
+// //task 5
+// var x = prompt ("enter a numeber" , 5 );
+// document.write ("<h1> Table of " + x + "</h1> "  + 
+//  x + " x 1= " + x*1 + "<br>" +x+ " x 2= "+x*2 + "<br> "+x+ " x 3= "+x*3 + "<br> "+x+" x 4= "+x*4 + "<br> "+x+ "x 5= "+x*5 + "<br>"+ x +" x 6= "+x*6 
+//  + "<br>"+ x  +" x 7= "+x*7+ "<br> "+ x+" x 8= "+x*8+ "<br>"+ x +" x 9= "+x*9+ " <br>"+ x + " x 10= "+x*10 );
+
+// //task 6
+// var x = prompt ("Enter the name of first subject" );
+// var y = prompt ("Enter the name of second subject" );
+// var z = prompt ("Enter the name of third subject" );
+// var a = +prompt ("Enter your obatined marks in " + x);
+// var b = +prompt ("Enter your obatined marks in " + y);
+// var c = +prompt ("Enter your obatined marks in " + z);
+// var d = a + b + c; 
+// var e = (d/total);
+// var f = (e*100);
+// var total = (100);
+// document.write ( "<table>"+ "<tr>"+ "<th>"+ "subject" +"</th>"+  "<th>"+ "total marks" +"</th>" 
+// +"<th>"+" marks obtained "+"</th>" + "<th>" +" percentage "+"</th>"+"</tr>"
+// + "<tr>"+ "<td>"+ x +"</td>" + "<td>"+ total +"</td>" + "<td>"+ a +"</td>" + "<td>"+ ((a/total)*100) +"</td>" +"</tr>"
+// + "<tr>"+ "<td>"+ y +"</td>" + "<td>"+ total +"</td>"+ "<td>"+ b +"</td>" + "<td>"+ ((b/total)*100) +"</td>" +"</tr>"
+// + "<tr>"+ "<td>"+ z +"</td>" + "<td>"+ total +"</td>"+ "<td>"+ c +"</td>" + "<td>"+ ((c/total)*100) +"</td>" +"</tr>"
+// + "<tr>"+ "<td>"+ " " +"</td>" + "<td>"+ total*3  +"</td>"+ "<td>"+ d +"</td>" + "<td>"+ f +"</td>" +"</tr>" 
+
+// +"</table>");
+
+
+// //chapter 9-11
+// //task 1
+// var city = prompt ("Enter the name of city");
+// if (city == "karachi") {alert ("welcome to the city of lights" );
+// }
+// else{
+//     alert("welcome to "+ city )
+// }
+
+// //task 2
+// var gender = prompt ("please mention your gender");
+// if (gender == "male") {alert ("Good Morning Sir" );
+// }
+// else if (gender == "female") {
+//     alert("Good Morning Ma’am");    
+// }
+// else {
+// alert("please check your spelling");
+// }
+
+// //task 3
+// var color = prompt ("mention a color of road traffic signal ");
+//  if (color == "red") {alert ("Must Stop" );
+//  }
+//  else if (color == "yellow") {
+//      alert("Ready to move");    
+//  }
+ 
+//  else if (color == "green") {
+//     alert("Move now");    
+// }
+//  else {
+//  alert("please check your spelling");
+//  }
+
+// //task 4
+// var fuel = prompt ("input remaining fuel in car (in litres)");
+// if (fuel < 0.25) {alert("Please refill the fuel in your car")
+// }
+
+// //task 5
+// var a = 4;
+//  if (++a === 5){ alert("given condition for variable a is true"); }   
+ 
+// var b = 82; 
+//  if (b++ === 83){ alert("given condition for variable b is true"); }  
+
+//  var c = 12; 
+// if (c++ === 13){ alert("condition 1 is true"); } 
+// if (c === 13){ alert("condition 2 is true"); }
+// if (++c < 14){ alert("condition 3 is true"); }
+// if(c === 14){ alert("condition 4 is true"); } 
+
+// var materialCost = 20000;
+// var laborCost = 2000; 
+// var totalCost = materialCost + laborCost; 
+// if (totalCost === laborCost + materialCost){ alert("The cost equals"); }  
+// if (true){ alert("True"); } if (false){ alert("False"); }  
+
+// if("car" < "cat"){ alert("car is smaller than cat"); }  
+
+// //task 7
+// var secret = 6;
+// var guess = prompt ("guessing game" , "guess the secret number");
+// if (guess == 6) { alert ( "Bingo! Correct answer" );}
+// else if  (guess == 7) { alert ( "Close enough to the correct answer" );}
+// else if (guess < 7) { alert ( "Wrong answer try again after refreshing the page" );}
+// else if (guess > 7) { alert ( "Wrong answer try again after refreshing the page" );}
